07c822d94872173928c4eadb1830a41fd8b5b5de
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2023-10-11 18:43:20 -0400

    feat: add-voltarButton

--- app/src/main/java/com/example/segundotrabalho/AdicionarTipo.java
+++ app/src/main/java/com/example/segundotrabalho/AdicionarTipo.java
@@ -3,6 +3,7 @@ package com.example.segundotrabalho;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AdicionarTipo extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class AdicionarTipo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tipo);
+    }
+
+    public void voltarButton(View v) {
+        this.finish();
     }
 }
