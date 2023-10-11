5313074db65adf8073b5ac9a3b53b0cfa0c28de2
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2023-10-11 17:09:47 -0400

    feat: add-menu-layout

---
+++ app/src/main/java/com/example/segundotrabalho/AdicionarTipo.java
@@ -0,0 +1,14 @@
+package com.example.segundotrabalho;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdicionarTipo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_tipo);
+    }
+}
