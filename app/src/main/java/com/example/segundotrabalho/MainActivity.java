c2038867f29cdf073113e918c5a467fa667f2f00
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2023-10-11 18:05:31 -0400

    feat: add-feature-abrirTelaAdicionarTipo

--- app/src/main/java/com/example/segundotrabalho/MainActivity.java
+++ app/src/main/java/com/example/segundotrabalho/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.segundotrabalho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonAdcionarTipo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void abrirTelaAdicionarTIpo(View v) {
+        Intent it_tela_adicionar_tipo = new Intent(this, AdicionarTipo.class);
+        startActivity(it_tela_adicionar_tipo);
     }
 }
