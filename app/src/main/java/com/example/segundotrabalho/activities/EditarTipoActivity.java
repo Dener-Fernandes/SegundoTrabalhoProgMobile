cb9eca8a965596ab39b088937f8ee102ea31a76c
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-10-14 03:43:12 -0400

    Padronizando strings.xml

--- app/src/main/java/com/example/segundotrabalho/activities/EditarTipoActivity.java
+++ app/src/main/java/com/example/segundotrabalho/activities/EditarTipoActivity.java
@@ -3,16 +3,48 @@ package com.example.segundotrabalho.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.segundotrabalho.R;
+import com.example.segundotrabalho.database.AppDatabase;
+import com.example.segundotrabalho.model.Tipo;
 
 public class EditarTipoActivity extends AppCompatActivity {
 
+    AppDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_tipo);
+    }
+
+    //NÃO ESTÁ FUNCIONANDO AINDA
+    public void adicionarTipo(View v) {
+        String tipoValue, descricaoValue;
+
+        int tipoId;
+
+        EditText tipoText = (EditText)findViewById(R.id.textTipoValueId);
+        EditText descricaoText = (EditText)findViewById(R.id.textDescricaoValueId);
+
+        tipoValue = tipoText.getText().toString();
+        descricaoValue = descricaoText.getText().toString();
+
+        Tipo tipo = new Tipo(tipoValue, descricaoValue);
+
+        db = AppDatabase.getAppDatabase(getApplicationContext());
+
+        long resultado = db.tipoDao().insertTipo(tipo);
+
+        if (resultado >= 0) {
+            Log.d("Inserção", "Inserção bem-sucedida. ID do objeto inserido: " + resultado);
+        }
+        else {
+            Log.e("Inserção", "Falha na inserção");
+        }
+
     }
     public void voltarButton(View v) {
         this.finish();
