8f9d03014325fa3f068f74233d92b595117d2bfe
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-10-14 02:40:57 -0400

    Levando para tela Editar tipo

--- app/src/main/java/com/example/segundotrabalho/MainActivity.java
+++ app/src/main/java/com/example/segundotrabalho/MainActivity.java
@@ -8,11 +8,13 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.segundotrabalho.activities.AdicionarTipoActivity;
+import com.example.segundotrabalho.activities.EditarTipoActivity;
 import com.example.segundotrabalho.activities.ListarObjetosActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     Button buttonAdcionarTipo;
+    Button buttonEditarTipo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     public void abrirTelaAdicionarTIpo(View v) {
         Intent it_tela_adicionar_tipo = new Intent(this, AdicionarTipoActivity.class);
         startActivity(it_tela_adicionar_tipo);
+    }
+
+    public void abrirTelaEditarTipo(View v) {
+        Intent it_tela_editar_tipo = new Intent(this, EditarTipoActivity.class);
+        startActivity(it_tela_editar_tipo);
     }
 
     public void abrirTelaListarObjetos(View v) {
---
+++ app/src/main/java/com/example/segundotrabalho/activities/EditarTipoActivity.java
@@ -0,0 +1,16 @@
+package com.example.segundotrabalho.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.segundotrabalho.R;
+
+public class EditarTipoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_tipo);
+    }
+}
