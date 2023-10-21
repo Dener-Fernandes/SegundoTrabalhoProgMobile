44f5fea6ccc463c76ac03198bfae8398f4efc5b8
Fernanda Pessoa <fernaandapessoa@outlook.com>
2023-10-21 03:33:43 -0400

    refactor: pasta de adapters

---
+++ app/src/main/java/com/example/segundotrabalho/activities/EditacaoObjetoActivity.java
@@ -0,0 +1,16 @@
+package com.example.segundotrabalho.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.segundotrabalho.R;
+
+public class EditacaoObjetoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editacao_objeto);
+    }
+}
